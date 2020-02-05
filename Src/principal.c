d86029e65810e5e2bf183e3e6e9356001e8e23ab
chipyards <noullet@chipyards.com>
2020-02-05 16:45:26 +0100

    machine a etats finis pour les niveaux de cheat code (apres 2s de maintien en M3)

--- Src/principal.c
+++ Src/principal.c
@@ -20,14 +20,28 @@ const static int coeff_detune[] = {
 //	27554,
 	26008 };
 */
+
+// arrondi optimal de N/D : ((2N/D)+1)/2
+// calcule a la compilation
+const static unsigned int periode_modu[] = {
+	((2*72000000/85000)+1)/2,
+	((2*72000000/90000)+1)/2,
+	((2*72000000/95000)+1)/2,
+	((2*72000000/100000)+1)/2,
+	((2*72000000/115000)+1)/2,
+	((2*72000000/120000)+1)/2,
+	((2*72000000/110000)+1)/2 };	// frequence hors-jeu
+
 unsigned int cnt10Hz = 0;
 int mode = 0;
-int freq = 0;
-int cheat_flag = 0;
+int freq = 0;		// index dans periode_modu
+int cheat_stat = 0;	// state machine state
+int cheat_tt = 0;	// target time : instant du prochain changement d'etat, en periodes systick 
 
-// durees exprimees en periode systick
-#define DUREE_LASER 1
-#define DUREE_LOCK  3	// prolongee eventuellement par le son
+// durees exprimees en periode systick (nominal 100ms)
+#define DUREE_LASER 	1
+#define DUREE_LOCK  	3	// duree de verrouillage mini avant prochain tir (avec ou sans son)
+#define DUREE_LOCK_MAX  600	// duree de verrouillage max en cheat mode
 
 // systick interrupt handler
 void sys_callback( void )
@@ -49,59 +63,89 @@ if	( cnt10Hz == DUREE_LASER )
 		audio_start();
 		gpio_init_audio();
 		}
+	return;
 	}
-if	( ( cnt10Hz >= DUREE_LOCK ) && ( !audio_is_playing() ) && ( !cheat_flag ) )
+if	( ( cnt10Hz >= DUREE_LOCK ) && ( !audio_is_playing() ) && ( cheat_stat == 0 ) )
 	{
-	gpio_power_off();
-	}
-// cheat code zone
-// mettre mode sur M3, attendre 2s que LED verte s'allume, alors lacher gachette
-if	( ( cnt10Hz == 20 ) && ( gpio_get_mode() == 2 ) )
-	{
-	cheat_flag = 1; gpio_init_gate();
-	gpio_led( VERT, 1 );
-	}
-// mettre mode sur M2, attendre 2s que LED rouge s'allume
-if	( ( cnt10Hz == 40 ) && ( cheat_flag ) )
-	{
-	gpio_led( VERT, 0 ); gpio_led( ROUGE, 1 );
-	}
-// attendre 1s que LED verte s'allume
-if	( ( cnt10Hz == 50 ) && ( cheat_flag ) )
-	{
-	gpio_led( ROUGE, 0 );
-	if	( gpio_get_mode() == 1 )
-		gpio_led( VERT, 1 );
-	else	{
-		cheat_flag = 0; gpio_power_off();
+	if	( gpio_get_mode() == 2 )	// il faut deja etre en M3 pour aller plus loin
+		{
+		cheat_stat = 1;
+		cheat_tt = cnt10Hz + 20;
 		}
-	}
-// mettre mode sur M1, attendre 1s que LED rouge s'allume
-if	( ( cnt10Hz == 60 ) && ( cheat_flag ) )
-	{
-	gpio_led( VERT, 0 ); gpio_led( ROUGE, 1 );	// passer en mode 0
-	}
-// attendre 1s que LED rouge s'eteigne et laser s'allume pour 29s
-if	( ( cnt10Hz == 70 ) && ( cheat_flag ) )
-	{
-	gpio_led( ROUGE, 0 );
-	if	( gpio_get_mode() == 0 )
-		gpio_laser_on();			// allumer le laser
-	else	gpio_power_off();
-	}
-if	( cnt10Hz == 360 )
 	gpio_power_off();
+	return;
+	}
+// fin de la zone de tir simple, on entre dans la cheat code state machine
+// tant que cnt10Hz < cheat_tt on ne fait rien
+if	( cnt10Hz != cheat_tt )
+	{
+	if	( cnt10Hz >= DUREE_LOCK_MAX )	// error catch
+		{ cheat_stat = 0; gpio_power_off(); }
+	return;
+	}
+// on est ici parceque cnt10Hz == cheat_tt
+switch	( cheat_stat )
+	{
+	// on avait mis mode sur M3, on a attendu 2s que LED verte s'allume, alors on peut lacher gachette
+	case 1 :
+	if	( gpio_get_mode() == 2 )
+		{
+		cheat_stat = ( freq + 1 ) * 100;
+		cheat_tt += 20;
+		gpio_init_gate();	// pour nous permettre de lacher la gachette, car DUREE_LOCK est passe
+		gpio_led( VERT, 1 );
+		}
+	break;
+	// le niveau de la frequence 1 : les 100
+	// mettre mode sur M2, dans 2s la LED rouge s'allume
+	case 100 :
+		{
+		cheat_stat++;
+		cheat_tt += 10;
+		gpio_led( VERT, 0 ); gpio_led( ROUGE, 1 );
+		}
+	break;
+	// dans 1s la LED verte s'allume
+	case 101 :
+		{
+		cheat_stat++;
+		cheat_tt += 10;
+		gpio_led( ROUGE, 0 );
+		if	( gpio_get_mode() == 1 )
+			gpio_led( VERT, 1 );
+		else	{
+			cheat_stat = 0; gpio_power_off();
+			}
+		}
+	break;
+	// alors metre mode sur M1, dans 1s la LED rouge s'allume
+	case 102 :
+		{
+		cheat_stat++;
+		cheat_tt += 10;
+		gpio_led( VERT, 0 ); gpio_led( ROUGE, 1 );
+		}
+	break;
+	// dans 1s la LED rouge s'eteint et laser s'allume pour 29s
+	case 103 :
+		{
+		cheat_stat++;
+		cheat_tt += 100;	// le temps de faire tests analog
+		gpio_led( ROUGE, 0 );
+		if	( gpio_get_mode() == 0 )
+			{
+			int resolution = PWM_Init_ff( TIM2, 3, periode_modu[6] );	// hors-jeu
+			TIM2->CCR3 = resolution / 2;		// a peu pres carre
+			gpio_laser_on();			// allumer le laser
+			}
+		else	gpio_power_off();
+		}
+	break;
+	default :
+		cheat_stat = 0; gpio_power_off();
+	}
 }
 
-// arrondi optimal de N/D : ((2N/D)+1)/2
-// calcule a la compilation
-const static unsigned int periode_modu[] = {
-	((2*72000000/85000)+1)/2,
-	((2*72000000/90000)+1)/2,
-	((2*72000000/95000)+1)/2,
-	((2*72000000/100000)+1)/2,
-	((2*72000000/115000)+1)/2,
-	((2*72000000/120000)+1)/2 };
 
 int main(void)
 {
